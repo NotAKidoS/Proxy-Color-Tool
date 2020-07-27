34267f5cf03f015d7a8439207af81a57dc7a6f42
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2020-07-26 23:19:47 -0500

    [Multiplayer] Proxy Colors on just spawned entities now work.
    Networking over the entities index value instead of itself so it can be checked multiple times over time to see if it becomes valid. Makes duplications on servers with proxy colors work. Applying proxy colors on an entity that just spawned now works too! (included a fallback for a 3 second check delay just in case)
    
    Code may be unneeded or redundent but I dont care as it now works and should work in most conditions.

--- lua/autorun/proxycolor_autorun.lua
+++ lua/autorun/proxycolor_autorun.lua
@@ -27,26 +27,26 @@ function Entity:SetProxyColor(ColorTable)
 	if ( CLIENT ) then
 		net.Start("NAKProxyColorSync")--network to player (unsure if this will work yet, maybe used for clientside derma menu preview)
 		net.WriteEntity(self)
-		net.WriteTable(ColorTable)
+			net.WriteTable(ColorTable)
 		net.Send(LocalPlayer())
 	else
-		net.Start("NAKProxyColorSync")--network to players
-		net.WriteEntity(self)
-		net.WriteTable(ColorTable)
-		net.Broadcast()
+		local entID = self:EntIndex()
+		if entID == -1 then
+			print("[Proxy Color] Attempted to network a server/clientside ONLY entity, refusing to send. https://bit.ly/2OY7Nyj")
+			print("[Proxy Color] If you believe this is a mistake or useless check, please contact me on steam. -NotAKid")
+		else
+			net.Start("NAKProxyColorSync")--network to players
+			net.WriteFloat(self:EntIndex())
+			net.WriteTable(ColorTable)
+			net.Broadcast()
+		end
 		duplicator.StoreEntityModifier( self, "proxycolor", ColorTable )
 	end
 end
 
 --Function to set color table for dupes
 function DupeSetProxyColor( ply, ent, CT )
-	--client needs to know of the entitys existence before networking happens THIS WILL FAIL IF PING IS TOO HIGH
-	if game.SinglePlayer() then
-		ent:SetProxyColor( CT )
-	else
-		ply:PrintMessage(HUD_PRINTTALK, "[Proxy Color] Players with high ping may not have proxy colors networked, please re-apply them if possible!")
-		timer.Simple( 1, function() ent:SetProxyColor( CT ) end )
-	end
+	ent:SetProxyColor( CT )
 end
 duplicator.RegisterEntityModifier( "proxycolor", DupeSetProxyColor )
 
@@ -55,14 +55,47 @@ if SERVER then
 	util.AddNetworkString("NAKProxyColorSync")
 else
 	net.Receive("NAKProxyColorSync",function()
-		local self = net.ReadEntity()
+		local entID = net.ReadFloat()
 		local CT = net.ReadTable()
-		self.ColorTable = CT
-		if CT[1] != nil then self.ColorSlot1 = CT[1] end
-		if CT[2] != nil then self.ColorSlot2 = CT[2] end
-		if CT[3] != nil then self.ColorSlot3 = CT[3] end
-		if CT[4] != nil then self.ColorSlot4 = CT[4] end
-		if CT[5] != nil then self.ColorSlot5 = CT[5] end
-		if CT[6] != nil then self.ColorSlot6 = CT[6] end
+		local TimerName = "PrxyClr_"..entID
+
+		--//Check if the entity exists a few times
+		timer.Create(TimerName, 0.01, 12, function(a,b)
+			local self = ents.GetByIndex( entID )
+			if IsValid(self) then 
+				timer.Remove(TimerName)
+				self.ColorTable = CT
+				if CT[1] != nil then self.ColorSlot1 = CT[1] end
+				if CT[2] != nil then self.ColorSlot2 = CT[2] end
+				if CT[3] != nil then self.ColorSlot3 = CT[3] end
+				if CT[4] != nil then self.ColorSlot4 = CT[4] end
+				if CT[5] != nil then self.ColorSlot5 = CT[5] end
+				if CT[6] != nil then self.ColorSlot6 = CT[6] end
+			end
+			--//if the players ping is too high, wait a bit and try again (less timer spam)
+			--//I tested with net_fakeloss and net_fakelag, no value ever triggered this. Unsure if this is needed but just in case.
+			if timer.RepsLeft(TimerName) == 0 then
+				print("[Proxy Color] Error receiving entity from server, too high of ping or entity does not exist! Retrying in 3 seconds...")
+				timer.Simple( 3, function() 
+					local self = ents.GetByIndex( entID )
+					if IsValid(self) then
+						if !self.ColorTable then
+							self.ColorTable = CT
+							if CT[1] != nil then self.ColorSlot1 = CT[1] end
+							if CT[2] != nil then self.ColorSlot2 = CT[2] end
+							if CT[3] != nil then self.ColorSlot3 = CT[3] end
+							if CT[4] != nil then self.ColorSlot4 = CT[4] end
+							if CT[5] != nil then self.ColorSlot5 = CT[5] end
+							if CT[6] != nil then self.ColorSlot6 = CT[6] end
+							print("[Proxy Color] Entity received, proxy colors have been set!")
+						else
+							print("[Proxy Color] Entity received, existing proxy colors found. Ignoring original colors...")
+						end
+					else
+						print("[Proxy Color] Entity failed to network, something may be missing its proxy colors for you. *THIS SHOULD NEVER HAPPEN ;-;*")
+					end
+				end )
+			end
+		end )
 	end)
 end
